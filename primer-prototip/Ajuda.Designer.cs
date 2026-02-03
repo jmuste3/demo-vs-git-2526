0d1a8b538b4640e7f2cf07e8220ca1ac5bb5aca6
JAUME <isard@WIN11-23H2>
2026-02-03 09:12:44 +0100

    sistema d'ajuda

--- primer-prototip/Ajuda.Designer.cs
+++ primer-prototip/Ajuda.Designer.cs
@@ -28,12 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Ajuda";
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(509, 152);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // Ajuda
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Name = "Ajuda";
+            Text = "Ajuda";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
     }
 }
